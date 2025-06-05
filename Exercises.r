8a2f06ef82c80016f3f889d9fe11c8365071f63d
James Broderick <pjimmythedude@gmail.com>
2025-06-04 21:41:08 -0400

    Switched last question var name to x

--- Exercises.r
+++ Exercises.r
@@ -1,5 +1,5 @@
 library(dslabs)
-male <- heights$height[heights$sex == "Male"]
+male <- heights$height[heights$sex=="Male"]
 female <- heights$height[heights$sex == "Female"]
 
 numMales <- length(male)
@@ -15,9 +15,10 @@ df <- data.frame(male_percentiles, female_percentiles)
 # 2. See above.
 # 3. Asia
 # 4. Africa
-# 5. 11 million
-# 6. b. 0.75
+# 5. ~11 million
+# 6. b. 0.75 (c. and d. are also correct but less accurate)
 # 7. Asia
 
-between_69_and_72 <- mean(male > 69 & male <= 72)
+x <- heights$height[heights$sex=="Male"]
+between_69_and_72 <- mean(x > 69 & x <= 72)
 # 8. ~0.334
